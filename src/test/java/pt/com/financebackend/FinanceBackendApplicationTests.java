c793e05715c5de8e5ba37a24016e7f3e8f62d03b
Marcelo Kath <marcelo.kath@mouts.info>
2020-04-12 13:14:25 +0000

    Initial commit.

---
+++ src/test/java/pt/com/financebackend/FinanceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.com.financebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
